678115811e5116a9fab6e2dc35d05e1f032f7543
redaphid <iam@aaronherres.com>
2019-08-08 00:33:52 -0700

    OMG

--- ravecom/ravecom.ino
+++ ravecom/ravecom.ino
@@ -2,16 +2,20 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include "esp_system.h"
+
 #define BAND 915E6 //you can set band here directly,e.g. 868E6,915E6
 #define SPREADING_FACTOR 10
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-byte localAddress;       // address of this device
-byte destination = 0xFF; // destination to send to
+#define MAX_INTERVAL 5000
+#define MIN_INTERVAL 1000
 
-String outgoing; // outgoing message
+byte localAddress;       // address of this device
+
+std::string outgoing; // outgoing message
 String msgString = "";
 byte msgCount = 0;     // count of outgoing messages
 long lastSendTime = 0; // last send time
@@ -32,15 +36,6 @@ void setup()
   LoRa.receive();
 }
 
-void getLocalAddress()
-{
-  const int point = 5;
-  for (int i = 0; i < 6; i++)
-  {
-    localAddress = 5;
-  }
-}
-
 void initScreen()
 {
   Heltec.display->init();
@@ -49,28 +44,38 @@ void initScreen()
   Heltec.display->display();
 }
 
-class BLECallback: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string value = pCharacteristic->getValue();
+class BLECallback : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    std::string value = pCharacteristic->getValue();
 
-      if (value.length() > 0) {
-        Serial.println("*********");
-        Serial.print("New value: ");
-        for (int i = 0; i < value.length(); i++)
-          Serial.print(value[i]);
-
-        Serial.println();
-        Serial.println("*********");
-      }
+    if (value.length() > 0)
+    {
+      Serial.print("received bluetooth value: ");              
+      Serial.println(value.c_str());      
     }
+    outgoing = value;
+  }
 };
+
+void getLocalAddress()
+{
+  uint8_t mac[6];
+  esp_efuse_mac_get_default(mac);
+  localAddress = 0;
+  for (int i = 0; i < 6; i++)
+  {
+    localAddress = ((int)localAddress + (int)mac[i]) % 256;
+  }
+}
 
 bool initBluetooth()
 {
 
+  getLocalAddress();
   BLEDevice::init("ravecom-" + localAddress);
   BLEServer *pServer = BLEDevice::createServer();
-
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
@@ -79,8 +84,6 @@ bool initBluetooth()
           BLECharacteristic::PROPERTY_WRITE);
 
   pCharacteristic->setCallbacks(new BLECallback());
-
-  pCharacteristic->setValue("Hello World");
   pService->start();
 
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
@@ -92,26 +95,54 @@ void loop()
   renderScreen();
   if (millis() - lastSendTime > interval)
   {
-    int before = millis();
-    sendMessage(outgoing);
-    lastSendTime = millis(); // timestamp the message
-    Serial.println("send time: " + String(millis() - before));
-    interval = random(5000) + 1000; // 2-3 seconds
+    sendMessage();   
+    interval = random(MAX_INTERVAL) + MIN_INTERVAL;
     LoRa.receive();                 // go back into receive mode
   }
 }
 
-void sendMessage(String outgoing)
-{
+void sendMessage()
+{   if(outgoing.length() == 0) return;
+    auto before = millis();
+    sendLora();
+
+    lastSendTime = millis();
+    auto timeItTook = lastSendTime - before;
+    sendSerial(timeItTook);
+    
+    if(timeItTook > MIN_INTERVAL) {
+      Serial.println("Message took too long to send. Resetting the message.");
+      outgoing = "err: msg-length";
+    }
+    
+}
+
+void sendLora() {
   digitalWrite(LED, LOW);
   LoRa.beginPacket();              // start packet
-  LoRa.write(destination);         // add destination address
   LoRa.write(localAddress);        // add sender address
   LoRa.write(msgCount);            // add message ID
   LoRa.write(outgoing.length());   // add payload length
-  LoRa.print(outgoing);            // add payload
+  LoRa.print(outgoing.c_str());            // add payload
   LoRa.endPacket();                // finish packet and send it
   msgCount = (msgCount + 1 % 256); // increment message ID
+}
+
+void sendSerial(int time) {
+  Serial.print("sent: ");
+  Serial.print(outgoing.c_str());
+
+  Serial.print(" addr: ");
+  Serial.print(localAddress);
+
+  Serial.print(" msg#: ");
+  Serial.print(msgCount);
+
+  Serial.print(" len: ");
+  Serial.print(outgoing.length());  
+  Serial.print(" time: ");
+  Serial.print(time);
+  Serial.println();  
 }
 
 void printScreen(String message)
@@ -124,7 +155,6 @@ void renderScreen()
 {
   if (msgString == "")
     return;
-  outgoing = msgString;
   Heltec.display->clear();
   Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
   Heltec.display->setFont(ArialMT_Plain_10);
